601b101acb4f6c7ce62f2bdb5077d93958003a9f
BOTz44 <106071317+BOTz44@users.noreply.github.com>
2022-09-27 23:18:02 +0700

    Update youtube-yta.js

--- plugins/youtube-yta.js
+++ plugins/youtube-yta.js
@@ -2,14 +2,10 @@ let limit = 80
 import fetch from 'node-fetch'
 import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
-let name = await conn.getName(who)
-
   if (!args || !args[0]) throw 'Uhm... urlnya mana?'
   let chat = global.db.data.chats[m.chat]
   const isY = /y(es)/gi.test(args[1])
-  const { thumbnail, audio: _audio, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
+  const { thumbnail, audio: _audio, title } = await youtubedlv2(args[0]).catch(async _ => await youtubedl(args[0])).catch(async _ => await youtubedlv3(args[0]))
   const limitedSize = (isPrems || isOwner ? 99 : limit) * 1024
   let audio, source, res, link, lastError, isLimit
   for (let i in _audio) {
@@ -37,26 +33,25 @@ let name = await conn.getName(who)
 *${htjava} Filesize:* ${audio.fileSizeH}
 
 *L O A D I N G. . .*
-`.trim(), m)
-  if (!isLimit) await conn.sendFile(m.chat, source, title + '.mp3', '', fakes, null, { fileLength: fsizedoc, seconds: fsizedoc, mimetype: 'audio/mp4', contextInfo: {
-          externalAdReply :{
-    body: 'Size: ' + audio.fileSizeH,
-    containsAutoReply: true,
-    mediaType: 2, 
-    mediaUrl: args[0],
-    showAdAttribution: true,
-    sourceUrl: args[0],
-    thumbnailUrl: thumbnail,
-    renderLargerThumbnail: true,
-    title: 'Nih Kak, ' + name,
-     }}
+`.trim(), m) // title + '.mp3',
+  if (!isLimit) await conn.sendFile(m.chat, source, title + 'audio/mpeg', `
+*${htki} YOUTUBE ${htka}*
+
+*${htjava} Title:* ${title}
+*${htjava} Type:* mp3
+*${htjava} Filesize:* ${audio.fileSizeH}
+
+*L O A D I N G. . .*
+`.trim(), m, null, {
+    asDocument: chat.useDocument
   })
 }
 handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url> <without message>`)
-handler.tags = ['downloader']
-handler.command = /^y((outube|tb)audio|(outube|tb?)mp3|utubaudio|taudio|ta)$/i
+handler.tags = ['downloader', 'limitmenu']
+handler.command = /^yt(a|mp3)$/i
 
 handler.exp = 0
-handler.register = false
+handler.register = true
+handler.limit = false
 
 export default handler
